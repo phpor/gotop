276c2dc8892a06cf1754ccbb1909a1dfa0d63113
phpor <ssologin56>
2018-08-13 10:53:11 +0800

    mem of container

--- src/widgets/mem.go
+++ src/widgets/mem.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/cjbassi/gotop/src/utils"
 	ui "github.com/cjbassi/termui"
-	psMem "github.com/shirou/gopsutil/mem"
+	"github.com/phpor/ctools/mem"
 )
 
 type Mem struct {
@@ -22,7 +22,6 @@ func NewMem(interval time.Duration, zoom int) *Mem {
 	self.Label = "Memory Usage"
 	self.Zoom = zoom
 	self.Data["Main"] = []float64{0}
-	self.Data["Swap"] = []float64{0}
 
 	self.update()
 
@@ -37,15 +36,11 @@ func NewMem(interval time.Duration, zoom int) *Mem {
 }
 
 func (self *Mem) update() {
-	main, _ := psMem.VirtualMemory()
-	swap, _ := psMem.SwapMemory()
-	self.Data["Main"] = append(self.Data["Main"], main.UsedPercent)
-	self.Data["Swap"] = append(self.Data["Swap"], swap.UsedPercent)
+	memstat,_ := mem.Usage()
+	memUsedPercent := float64(memstat.Used)/float64((memstat.Total) * 100)
+	self.Data["Main"] = append(self.Data["Main"], memUsedPercent)
 
-	mainTotalBytes, mainTotalMagnitude := utils.ConvertBytes(main.Total)
-	swapTotalBytes, swapTotalMagnitude := utils.ConvertBytes(swap.Total)
-	mainUsedBytes, mainUsedMagnitude := utils.ConvertBytes(main.Used)
-	swapUsedBytes, swapUsedMagnitude := utils.ConvertBytes(swap.Used)
-	self.Labels["Main"] = fmt.Sprintf("%3.0f%% %.0f%s/%.0f%s", main.UsedPercent, mainUsedBytes, mainUsedMagnitude, mainTotalBytes, mainTotalMagnitude)
-	self.Labels["Swap"] = fmt.Sprintf("%3.0f%% %.0f%s/%.0f%s", swap.UsedPercent, swapUsedBytes, swapUsedMagnitude, swapTotalBytes, swapTotalMagnitude)
+	mainTotalBytes, mainTotalMagnitude := utils.ConvertBytes(memstat.Total)
+	mainUsedBytes, mainUsedMagnitude := utils.ConvertBytes(memstat.Used)
+	self.Labels["Main"] = fmt.Sprintf("%3.0f%% %.0f%s/%.0f%s", memUsedPercent, mainUsedBytes, mainUsedMagnitude, mainTotalBytes, mainTotalMagnitude)
 }
